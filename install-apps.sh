e0f113a98b98bb0782ecf9314e5348b60d7f2357
Jorge O. Castro <jorge.castro@gmail.com>
2021-07-12 19:42:59 -0400

    Initial commit

---
+++ install-apps.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+## Install applications from flathub and flathub-beta. 
+## Intended for Fedora Silverblue and openSUSE MicroOS
+set -ex
+[ "$UID" -eq 0 ] || { echo "This script must be run as root."; exit 1;} # Need to figure out how to pkexec so we only ask for the password once.
+
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak remote-add --if-not-exists flathub-beta https://flathub.org/beta-repo/flathub-beta.flatpakrepo
+
+## Ignore a line if it starts with # so you can leave yourself notes
+grep -vE '^#' applications.list | xargs sudo flatpak install flathub --assumeyes --noninteractive
+grep -vE '^#' applications-beta.list | xargs sudo flatpak install flathub-beta --assumeyes --noninteractive
