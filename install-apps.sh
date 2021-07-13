db15b98e08bbdc28e125dd9a7282f67eadd7ff19
Jorge O. Castro <jorge.castro@gmail.com>
2021-07-12 20:07:11 -0400

    Add full path

--- install-apps.sh
+++ install-apps.sh
@@ -8,5 +8,9 @@ flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flat
 flatpak remote-add --if-not-exists flathub-beta https://flathub.org/beta-repo/flathub-beta.flatpakrepo
 
 ## Ignore a line if it starts with # so you can leave yourself notes
-grep -vE '^#' applications.list | xargs sudo flatpak install flathub --assumeyes --noninteractive
-grep -vE '^#' applications-beta.list | xargs sudo flatpak install flathub-beta --assumeyes --noninteractive
+grep -vE '^#' applications.list | xargs sudo /usr/bin/flatpak install flathub --assumeyes --noninteractive
+grep -vE '^#' applications-beta.list | xargs sudo /usr/bin/flatpak install flathub-beta --assumeyes --noninteractive
+
+## Remove Firefox from the base image
+# rpm-ostree override remove firefox
+# echo "You should reboot!"
