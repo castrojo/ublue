c89ce895a3f22d9dbc87e7754da99f29d08d7a4a
Jorge O. Castro <jorge.castro@gmail.com>
2021-11-03 18:27:02 -0400

    Add idempotent switch

--- 02-workarounds.sh
+++ 02-workarounds.sh
@@ -6,7 +6,7 @@ echo "Add VSCode as an overlay"
 # vscode via a flatpak that can access system/toolbox environments isn't a good UX yet, so we compromise here:
 
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
-rpm-ostree install code
+rpm-ostree --idempotent install code
 
 # If you need Docker in addition to podman
 #rpm-ostree install moby-engine docker-compose
@@ -16,7 +16,7 @@ rpm-ostree install code
 # this if you want the Ubuntu theme 
 #
 echo "Adding the Ubuntu yaru theme"
-rpm-ostree install yaru-theme
+rpm-ostree --idempotent install yaru-theme
 gsettings set org.gnome.desktop.wm.preferences theme "Yaru"
 gsettings set org.gnome.desktop.interface gtk-theme "Yaru"
 gsettings set org.gnome.desktop.interface icon-theme "Yaru"
