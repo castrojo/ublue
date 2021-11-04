d01f2afbb0b828b4d1f60306523351cbd9456560
Marco Ceppi <marco@stacklet.io>
2021-11-03 21:51:30 -0400

    fix: add font cache flush

--- 02-workarounds.sh
+++ 02-workarounds.sh
@@ -36,6 +36,8 @@ mkdir -p ~/.local/share/fonts
 curl -sfLo "$HOME/.local/share/fonts/Ubuntu Mono Nerd Font Complete.ttf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/UbuntuMono/Regular/complete/Ubuntu%20Mono%20Nerd%20Font%20Complete.ttf
 curl -sfLo "$HOME/.local/share/fonts/Ubuntu Nerd Font Complete.ttf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Ubuntu/Regular/complete/Ubuntu%20Nerd%20Font%20Complete.ttf
 cp -R files/fonts/ubuntu-font-family/*.ttf $HOME/.local/share/fonts/
+# Flush font cache
+fc-cache
 
 # Get current profile for terminal, and update the font
 current_profile=$(dconf list /org/gnome/terminal/legacy/profiles:/ | head -n1)
